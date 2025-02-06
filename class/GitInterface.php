657aa5ae9ddf7316c69dc3f3cb94f9af98f4a110
atm-etienne <etienne.bender@atm-consulting.fr>
2025-02-06 17:39:40 +0100

    New GitInterface class

---
+++ class/GitInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+abstract class GitInterface implements GitStatusCodeInterface {
+	protected const TOKEN_CONST_NAME = "GIT_GIT_TOKEN";
+	private static ?string $token = null;
+	private array $headers = [];
+
+	public function __construct(
+		private string $repository,
+		private string $owner,
+		private string $baseUrl
+	) {
+		$this->headers = [
+			'Authorization' => 'Bearer '.self::getToken(),
+			'User-Agent' 	=> 'Git Connector',
+		];
+	}
+
+	public abstract function getRepoPullRequestList(): array;
+	public abstract function createRepositoryBranch(string $branchName): array;
+	public abstract function getBranchUrl(string $branchName): string;
+	public abstract function getBranchSha(string $branchName): string|bool;
+
+	protected function addHeaders(array $headers): void {
+		$this->headers = array_merge($this->headers, $headers);
+	}
+}
