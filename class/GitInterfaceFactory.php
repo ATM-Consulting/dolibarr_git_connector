b50875d18798cb7f1fa1c28bd0d74ed612ad0cd8
atm-etienne <etienne.bender@atm-consulting.fr>
2025-02-06 17:59:46 +0100

    New GitInterfaceFactory

---
+++ class/GitInterfaceFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+class GitInterfaceFactory {
+
+	public static function create(GitPlatform $platform, $repo, $owner, $baseUrl): GitInterface {
+		return match ($platform) {
+			GitPlatform::GITHUB	=> new GitHubInterface($repo, $owner, $baseUrl),
+			GitPlatform::GITLAB	=> new GitLabInterface($repo, $owner, $baseUrl),
+		};
+	}
+
+}
